2bac4001e937672bd4d6cb81402314b5e39c4019
Krzysztof Zieliński <kzielinski50@gmail.com>
2025-02-07 19:41:34 +0100

    Add userSlice for managing user state and asynchronous data fetching

---
+++ redux/userSlice.js
@@ -0,0 +1,125 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import api from '../lib/api';
+
+export const fetchUserData = createAsyncThunk(
+  'user/fetchUserData',
+  async (userId, { rejectWithValue }) => {
+    try {
+      const response = await api.get(`/api/user/get`, { params: { userId } });
+      console.log('response', response);
+      if (!response.ok) {
+        throw new Error('Failed to fetch user data');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+const initialState = {
+  currentUser: null,
+  loading: false,
+  error: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+    },
+    signInSuccess: (state, action) => {
+      // action.payload is the user object
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+    signOutUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    signOutUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    updateOnboarding(state, action) {
+      if (state.currentUser) {
+        state.currentUser.isOnboardingCompleted = action.payload;
+      }
+    },
+    updateProfileFilled(state, action) {
+      if (state.currentUser) {
+        state.currentUser.isProfileFilled = action.payload;
+      }
+    },
+    extraReducers: (builder) => {
+      builder
+        .addCase(fetchUserData.pending, (state) => {
+          state.loading = true;
+        })
+        .addCase(fetchUserData.fulfilled, (state, action) => {
+          state.currentUser = action.payload;
+          state.loading = false;
+          state.error = null;
+        })
+        .addCase(fetchUserData.rejected, (state, action) => {
+          state.loading = false;
+          state.error = action.payload;
+        });
+    },
+  },
+});
+
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+  signOutUserStart,
+  signOutUserSuccess,
+  signOutUserFailure,
+  updateOnboarding,
+  updateProfileFilled,
+} = userSlice.actions;
+
+export default userSlice.reducer;
